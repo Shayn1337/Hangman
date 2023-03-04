aaf5d3bb16a6a387f7cf87f0fa7b0c6bd0b2f107
Shayn1337 <98117608+Shayn1337@users.noreply.github.com>
2023-03-04 15:27:02 +0100

    Add files via upload
    Version 0.95
    Neue Wortlisten (Eng+Deu beide 800+)
    Tastaturlock zwischen den runden
    Vollbild entfernt

--- hangman.py
+++ hangman.py
@@ -2,8 +2,19 @@ import tkinter as tk
 from string import ascii_uppercase
 import random
 import json
+import sys, os
 
-with open('config', 'r') as f:
+game = 0
+
+if getattr(sys, 'frozen', False):
+    bundle_dir = sys._MEIPASS
+else:
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
+configfile = bundle_dir + "\config"
+word_list_d = bundle_dir + "\word_list_d"
+word_list_e = bundle_dir + "\word_list_e"
+
+with open(configfile, 'r') as f:
     config = json.load(f)
 
 configvar = ["language","win","loss"]
@@ -11,12 +22,8 @@ configvar = ["language","win","loss"]
 for f in configvar:
     if f not in config:
         config[f] = 0
-with open('config', 'w') as f:
+with open(configfile, 'w') as f:
     json.dump(config, f)
-
-
-
-
 
 class App(tk.Tk):
     def __init__ (self):
@@ -43,16 +50,10 @@ class StartPage(tk.Frame):
 
 #Spiel Frame
 class StartGame(tk.Frame):
-
-
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Spiel gestartet").grid(columnspan=9)
         #hier muss das Spiel rein:
-
-#        def newGame():
-
-
 
         def generateword():
             global word_with_space
@@ -60,17 +61,17 @@ class StartGame(tk.Frame):
             global rndword
             global line_word
             global life
+            global game
             length = 0
             life = 12
+            game = 1
 
             if config['language'] == 1:
-                wordlist="word_list_d"
+                wordlist=word_list_d
             elif config['language'] == 2:
-                wordlist="word_list_e"
+                wordlist=word_list_e
             else:
-                wordlist="word_list_d"
-
-            
+                wordlist=word_list_d
 
             word=[]
             wordlist = open(wordlist,'r')
@@ -87,33 +88,35 @@ class StartGame(tk.Frame):
             LabelLife['text'] = ("verbleibende Leben: {}".format(life))
             Label['text'] = line_word
 
-
         def guess(select_letter):
             select_letter = select_letter.lower()
             global rndword
             global line_word
             global life
-            if(rndword.lower().count(select_letter) > 0):
-                for x in range(rndword.lower().count(select_letter)):
-                    line_word = line_word[:rndword.lower().find(select_letter)] + rndword[rndword.lower().find(select_letter)] + line_word[(rndword.lower().find(select_letter)+1):]
-                    rndword = rndword[:rndword.lower().find(select_letter)] + "-" + rndword[(rndword.lower().find(select_letter)+1):]
-                if line_word.count("_") < 1:
-                    line_word = ("Glückwunsch das Wort ist richtig: \n {}".format(line_word))
-                    config['win'] += 1
-                    with open('config', 'w') as f:
-                        json.dump(config, f)
-            else:
-                life-=1
-                if life < 1 :
-                    line_word = "Bitte probier es erneut"
-                    config['loss'] += 1
-                    with open('config', 'w') as f:
-                        json.dump(config, f)
+            global game
+            if game == 1:
+                if(rndword.lower().count(select_letter) > 0):
+                    for x in range(rndword.lower().count(select_letter)):
+                        line_word = line_word[:rndword.lower().find(select_letter)] + rndword[rndword.lower().find(select_letter)] + line_word[(rndword.lower().find(select_letter)+1):]
+                        rndword = rndword[:rndword.lower().find(select_letter)] + "-" + rndword[(rndword.lower().find(select_letter)+1):]
+                    if line_word.count("_") < 1:
+                        line_word = ("Glückwunsch das Wort ist richtig: \n {}".format(line_word))
+                        config['win'] += 1
+                        game = 0
+                        with open(configfile, 'w') as f:
+                            json.dump(config, f)
+                else:
+                    life-=1
+                    if life < 1 :
+                        line_word = "Bitte probier es erneut"
+                        config['loss'] += 1
+                        game = 0
+                        with open(configfile, 'w') as f:
+                            json.dump(config, f)
             
-            
-            LabelLife['text'] = ("verbleibende Leben: {}".format(life))
-            Label['text'] = line_word
-
+        
+                LabelLife['text'] = ("verbleibende Leben: {}".format(life))
+                Label['text'] = line_word
 
         Label = tk.Label(self, text="Generiere ein Word!", font=10, width=90)
         LabelLife = tk.Label(self, text="Drücke Neues Spiel um ein Spiel zu starten", font=10, width=90)
@@ -134,7 +137,7 @@ class Options(tk.Frame):
 
         def language(number):
             config['language'] = number
-            with open('config', 'w') as f:
+            with open(configfile, 'w') as f:
                 json.dump(config, f)
 
         tk.Frame.__init__(self, master)
@@ -150,7 +153,11 @@ class Options(tk.Frame):
 class Highscore(tk.Frame):
     def __init__(self, master):
 
-        win_loss_rate = round((config['win']/(config['win']+config['loss'])),2)
+        if (config['win'] ==0)and(config['loss'] == 0):
+            win_loss_rate = 0
+        else:      
+            win_loss_rate = round((config['win']/(config['win']+config['loss'])),2)
+        
         tk.Frame.__init__(self, master)
         tk.Frame.configure(self)
         tk.Label(self, text="Highscore", width='20').grid(column=1, columnspan=2, pady=15)
@@ -164,5 +171,6 @@ class Highscore(tk.Frame):
 
 if __name__ == "__main__":
     app = App()
-    app.attributes('-fullscreen',True)
+    app.geometry("1000x300")
+    app.title("Hangman")
     app.mainloop()
